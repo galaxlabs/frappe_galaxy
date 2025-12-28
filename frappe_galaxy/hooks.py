42f114d91a1f5ccb722e14731f11dcc32b05bbaf
galaxlabs <galaxylab2020@gmail.com>
2025-12-28 20:55:09 +0000

    hooks update

--- frappe_galaxy/hooks.py
+++ frappe_galaxy/hooks.py
@@ -38,17 +38,67 @@ add_to_apps_screen = [
 # include js, css files in header of web form
 # webform_include_js = {"doctype": "public/js/doctype.js"}
 # webform_include_css = {"doctype": "public/css/doctype.css"}
-
 website_route_rules = [
+    # Doc
     {"from_route": "/docs", "to_route": "content_list", "defaults": {"content_type": "Doc"}},
-    {"from_route": "/docs/<category>", "to_route": "content_list", "defaults": {"content_type": "Doc"}},
+    {"from_route": "/docs/category/<category>", "to_route": "content_list", "defaults": {"content_type": "Doc"}},
+
+    # Tutorial
     {"from_route": "/tutorials", "to_route": "content_list", "defaults": {"content_type": "Tutorial"}},
-    {"from_route": "/tutorials/<category>", "to_route": "content_list", "defaults": {"content_type": "Tutorial"}},
+    {"from_route": "/tutorials/category/<category>", "to_route": "content_list", "defaults": {"content_type": "Tutorial"}},
+
+    # Wiki
     {"from_route": "/wiki", "to_route": "content_list", "defaults": {"content_type": "Wiki"}},
-    {"from_route": "/wiki/<category>", "to_route": "content_list", "defaults": {"content_type": "Wiki"}},
+    {"from_route": "/wiki/category/<category>", "to_route": "content_list", "defaults": {"content_type": "Wiki"}},
+
+    # Note
+    {"from_route": "/notes", "to_route": "content_list", "defaults": {"content_type": "Note"}},
+    {"from_route": "/notes/category/<category>", "to_route": "content_list", "defaults": {"content_type": "Note"}},
+
+    # Profile
+    {"from_route": "/profiles", "to_route": "content_list", "defaults": {"content_type": "Profile"}},
+    {"from_route": "/profiles/category/<category>", "to_route": "content_list", "defaults": {"content_type": "Profile"}},
+
+    # Article
+    {"from_route": "/articles", "to_route": "content_list", "defaults": {"content_type": "Article"}},
+    {"from_route": "/articles/category/<category>", "to_route": "content_list", "defaults": {"content_type": "Article"}},
+
+    # HowTo
+    {"from_route": "/howto", "to_route": "content_list", "defaults": {"content_type": "HowTo"}},
+    {"from_route": "/howto/category/<category>", "to_route": "content_list", "defaults": {"content_type": "HowTo"}},
+
+    # FAQ
+    {"from_route": "/faq", "to_route": "content_list", "defaults": {"content_type": "FAQ"}},
+    {"from_route": "/faq/category/<category>", "to_route": "content_list", "defaults": {"content_type": "FAQ"}},
+
+    # Portfolio
+    {"from_route": "/portfolio", "to_route": "content_list", "defaults": {"content_type": "Portfolio"}},
+    {"from_route": "/portfolio/category/<category>", "to_route": "content_list", "defaults": {"content_type": "Portfolio"}},
+
+    # Jobs
+    {"from_route": "/jobs", "to_route": "content_list", "defaults": {"content_type": "Jobs"}},
+    {"from_route": "/jobs/category/<category>", "to_route": "content_list", "defaults": {"content_type": "Jobs"}},
+
+    # Projects
+    {"from_route": "/projects", "to_route": "content_list", "defaults": {"content_type": "Projects"}},
+    {"from_route": "/projects/category/<category>", "to_route": "content_list", "defaults": {"content_type": "Projects"}},
+
+    # Post
     {"from_route": "/posts", "to_route": "content_list", "defaults": {"content_type": "Post"}},
-    {"from_route": "/posts/<category>", "to_route": "content_list", "defaults": {"content_type": "Post"}},
+    {"from_route": "/posts/category/<category>", "to_route": "content_list", "defaults": {"content_type": "Post"}},
 ]
+
+
+# website_route_rules = [
+#     {"from_route": "/docs", "to_route": "content_list", "defaults": {"content_type": "Doc"}},
+#     {"from_route": "/docs/<category>", "to_route": "content_list", "defaults": {"content_type": "Doc"}},
+#     {"from_route": "/tutorials", "to_route": "content_list", "defaults": {"content_type": "Tutorial"}},
+#     {"from_route": "/tutorials/<category>", "to_route": "content_list", "defaults": {"content_type": "Tutorial"}},
+#     {"from_route": "/wiki", "to_route": "content_list", "defaults": {"content_type": "Wiki"}},
+#     {"from_route": "/wiki/<category>", "to_route": "content_list", "defaults": {"content_type": "Wiki"}},
+#     {"from_route": "/posts", "to_route": "content_list", "defaults": {"content_type": "Post"}},
+#     {"from_route": "/posts/<category>", "to_route": "content_list", "defaults": {"content_type": "Post"}},
+# ]
 
 
 # include js in page
